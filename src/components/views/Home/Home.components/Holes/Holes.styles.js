1faadbad8bbbf3c462c3a618b9fc28b2fdb732dc
Bogdan <bogdan_kolesov@bk.ru>
2022-01-23 10:45:26 +0600

    fix: Halls

--- src/components/views/Home/Home.components/Holes/Holes.styles.js
+++ src/components/views/Home/Home.components/Holes/Holes.styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
-import { COLOR_DARK, COLOR_GREEN, COLOR_ORANGE, COLOR_WHITE } from '../../../../../constants/colors';
+import { COLOR_DARK, COLOR_GRAY, COLOR_GREEN, COLOR_LITEGRAY, COLOR_ORANGE, COLOR_WHITE } from '../../../../../constants/colors';
 import Slider from "react-slick";
+import { Button } from '../../../../atoms';
 
 export const HolesContainer = styled.section`
     width: 100%;
@@ -13,13 +14,22 @@ export const HolesTitles = styled.div`
     display: flex;
     width: 100%;
     justify-content: space-between;
+    align-items: center;
+    margin-bottom: 35px;
     h2, a {
         color: ${COLOR_DARK};
+    }
+    h2{
+        font-size: 60px;
+    }
+    a{
+        font-size: 18px;
+        text-decoration: underline;
     }
 `
 export const HolesSlider = styled(Slider)`
     display: flex;
-    width: 90%;
+    width: 80%;
     justify-content: space-between;
 `
 
@@ -27,7 +37,10 @@ export const HoleItem = styled.div`
     width: 30%;
     display: flex;
     flex-direction: column;
-    padding: 20px;
+    /* margin: 20px; */
+    border: 1px solid ${COLOR_GRAY};
+    border-radius: 5px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 0px 2px rgba(0, 0, 0, 0.06), 0px 0px 1px rgba(0, 0, 0, 0.04);
 `
 export const HoleImage = styled.div`
     width: 100%;
@@ -57,15 +70,20 @@ export const HoleOffer = styled.a`
 export const HoleItemContent = styled.div`
     display: flex;
     flex-direction: column;
-
+    margin: 20px;
     h4{
         font-size: 20px;
         color: ${COLOR_DARK};
-        margin: 20px;
+        margin-bottom: 20px;
+    }
+
+    ${Button}{
+        margin-top: 10px;
     }
 `
 export const HoleItemText = styled.p`
     color: ${COLOR_DARK};
+    margin-bottom: 15px;
     span{
         color: ${COLOR_GREEN};
         margin-right: 2px;
