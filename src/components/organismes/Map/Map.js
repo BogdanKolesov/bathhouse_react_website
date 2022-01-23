1ad466ba4a7ae624a9592db780156e2419f9cb92
Bogdan <bogdan_kolesov@bk.ru>
2022-01-24 00:00:34 +0600

    fix: Map

--- src/components/organismes/Map/Map.js
+++ src/components/organismes/Map/Map.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { TileLayer, Marker, Popup } from 'react-leaflet'
-import { AppContainer } from '../../atoms';
-import { StyledMapContainer } from './Map.styles';
+import { AppContainer, Button } from '../../atoms';
+import { MapContacts, MapContactsAdress, MapContactsPhone, MapContactsSocial, MapContactsTime, StyledMapContainer } from './Map.styles';
 
 const Map = () => {
     const position = [59.921719, 30.348540]
@@ -14,9 +14,36 @@ const Map = () => {
                 />
                 <Marker position={position}>
                     <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
+                        Баня "Берёзка"
                     </Popup>
                 </Marker>
+                <MapContacts>
+                    <MapContactsAdress>
+                        <div>
+                            <img src='/assets/icons/mapPoint.svg' alt='' />
+                            <p>Наш адрес: город Архангельск,
+                                СНТ Транспортник, стр.11</p>
+                        </div>
+                    </MapContactsAdress>
+                    <MapContactsPhone>
+                        <div>
+                            <img src='/assets/icons/phone.svg' alt='' />
+                            <p>Телефон: +7 (911) 554-33-44</p>
+                        </div>
+                    </MapContactsPhone>
+                    <MapContactsTime>
+                        <div>
+                            <img src='/assets/icons/clock.svg' alt='' />
+                            <p>График работы: круглосуточно</p>
+                        </div>
+                    </MapContactsTime>
+                    <MapContactsSocial>
+
+                    </MapContactsSocial>
+                    <Button>
+                        Зказать обратный звонок
+                    </Button>
+                </MapContacts>
             </StyledMapContainer>
         </AppContainer>
     );
--- src/components/organismes/Map/Map.styles.js
+++ src/components/organismes/Map/Map.styles.js
@@ -1,7 +1,48 @@
 import styled from 'styled-components';
 import { MapContainer } from 'react-leaflet'
+import { COLOR_DARK, COLOR_WHITE } from '../../../constants/colors';
 
 export const StyledMapContainer = styled(MapContainer)`
     margin-top: 80px;
     height: 500px;
+    position: relative;
+`
+export const MapContacts = styled.div`
+    background-color: ${COLOR_WHITE};
+    border-radius: 5px;
+    position: absolute;
+    top: 50px;
+    /* bottom: 0; */
+    left: 320px;
+    z-index: 999;
+    height: 400px;
+    padding: 33px 0 44px 70px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+export const MapContactsAdress = styled.p`
+    width: 60%;
+    div{
+        display: flex;
+        img{
+            margin-right: 15px;
+        }
+        p{
+            color: ${COLOR_DARK};
+            font-size: 20px;
+        }
+
+    }
+`
+
+export const MapContactsPhone = styled(MapContactsAdress)`
+
+`
+export const MapContactsTime = styled(MapContactsAdress)`
+
+`
+
+export const MapContactsSocial = styled.div`
+
 `
