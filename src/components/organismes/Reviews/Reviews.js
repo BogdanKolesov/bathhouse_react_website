ed55ceff97cca3d16f0ff09de0217a236576c6f4
Bogdan <bogdan_kolesov@bk.ru>
2022-01-23 22:57:44 +0600

    add: Reviews

--- src/components/organismes/Reviews/Reviews.js
+++ src/components/organismes/Reviews/Reviews.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { AppContainer, Button } from '../../atoms';
+import { Divider, Rating, RatingStarIcon, ReviewsContainer, ReviewsItem, ReviewsItemButtons, ReviewsItemDate, ReviewsItemName, ReviewsItems, ReviewsItemText } from './Reviews.styles';
 
-const Reviews = () => {
+const Reviews = ({ content }) => {
     return (
-        <div>
-
-        </div>
+        <AppContainer>
+            <ReviewsContainer>
+                <h2>
+                    Отзывы
+                </h2>
+                <ReviewsItems>
+                    {
+                        content.map((item, index) => (
+                            <ReviewsItem key={index}>
+                                <ReviewsItemDate>
+                                    {item.date}
+                                </ReviewsItemDate>
+                                <Rating>
+                                    <RatingStarIcon src='/assets/icons/starYellow.svg' alt='' />
+                                    <RatingStarIcon src='/assets/icons/starYellow.svg' alt='' />
+                                    <RatingStarIcon src='/assets/icons/starYellow.svg' alt='' />
+                                    <RatingStarIcon src='/assets/icons/starYellow.svg' alt='' />
+                                    <RatingStarIcon src='/assets/icons/starYellow.svg' alt='' />
+                                </Rating>
+                                <ReviewsItemName>
+                                    {item.name}
+                                </ReviewsItemName>
+                                <ReviewsItemText>
+                                    {item.review}
+                                </ReviewsItemText>
+                                <Divider />
+                            </ReviewsItem>
+                        ))
+                    }
+                    <ReviewsItemButtons>
+                        <Button>Смотреть ещё</Button>
+                        <Button white >Оставить отзыв</Button>
+                    </ReviewsItemButtons>
+                </ReviewsItems>
+            </ReviewsContainer>
+        </AppContainer>
     );
 }
 
--- src/components/organismes/Reviews/Reviews.styles.js
+++ src/components/organismes/Reviews/Reviews.styles.js
@@ -1,1 +1,63 @@
 import styled from 'styled-components';
+import { COLOR_DARK, COLOR_LITEGRAY } from '../../../constants/colors';
+import { Button } from '../../atoms';
+
+export const ReviewsContainer = styled.section`
+    margin-top: 80px;
+    
+    h2{
+        color: ${COLOR_DARK};
+        font-size: 60px;
+    }
+`
+export const ReviewsItems = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const ReviewsItem = styled.div`
+    margin-top: 42px;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+`
+export const Divider = styled.div`
+    width: 100%;
+    height: 1px;
+    background-color: rgba(75, 75, 75, 0.7);
+    margin-top: 30px;
+`
+export const ReviewsItemDate = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    font-size: 14px;
+    color: ${COLOR_LITEGRAY};
+`
+export const ReviewsItemIcon = styled.div`
+
+`
+export const Rating = styled.div`
+
+`
+export const RatingStarIcon = styled.img`
+
+`
+
+export const ReviewsItemName = styled.div`
+    margin-top: 12px;
+    font-size: 20px;
+    font-weight: bold;
+`
+export const ReviewsItemText = styled.div`
+    margin-top: 10px;
+    font-size: 16px;
+`
+
+export const ReviewsItemButtons = styled.div`
+    margin: 0 auto;
+    margin-top: 50px;
+
+    ${Button}:first-child{
+        margin-right: 40px;
+    }
+`
